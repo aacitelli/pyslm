b0ef86bf2e09fa8367529f6edcd5df1f814b8df1
John Cook <74627489+jjocook@users.noreply.github.com>
2022-01-05 12:22:43 -0500

    reference and indexing errors corrected

--- pyslm/hatching/__init__.py
+++ pyslm/hatching/__init__.py
@@ -2,3 +2,4 @@ from .sorting import AlternateSort, GreedySort, LinearSort
 from .hatching import BaseHatcher, Hatcher, BasicIslandHatcher, InnerHatchRegion, StripeHatcher, getExposurePoints
 from .islandHatcher import Island, IslandHatcher
 from .utils import *
+import pyclipper
